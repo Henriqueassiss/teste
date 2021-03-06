86575d735f199752a1dbf456830dc4ac34e53fec
Henrique Assis <contato.henriqueassis@outlook.com>
2021-03-06 20:17:35 -0300

    update menu button

--- js/script.js
+++ js/script.js
@@ -1,37 +1,35 @@
-// Hide Or Show On Mobile & Desktop Itens
-const body = document.querySelector("body").classList;
-const header = document.querySelector(".header").classList;
-const menuBtn = document.querySelector(".header__toggle").classList;
-const menuLinks = document.querySelector(".header__menu").classList;
+const header = document.querySelector(".header");
+const menuBtn = document.querySelector(".header__toggle");
+const menuLinks = document.querySelector(".header__menu");
 
 // Show Menu On Mobile Devices
 const showMenu = () => {
-  if (menuBtn.contains("active")) {
-    body.remove("no-scroll");
-    menuBtn.remove("active");
-    menuLinks.remove("active");
+  console.log("clicked");
+  if (menuBtn.classList.contains("active")) {
+    menuBtn.classList.remove("active");
+    menuLinks.classList.remove("active");
   } else {
-    body.add("no-scroll");
-    menuBtn.add("active");
-    menuLinks.add("active");
-    window.onclick = function (event) {
-      if (!event.target.matches(".header__toggle")) {
-        body.remove("no-scroll");
-        menuBtn.remove("active");
-        menuLinks.remove("active");
-      }
-    };
+    menuBtn.classList.add("active");
+    menuLinks.classList.add("active");
   }
+  window.onclick = function (event) {
+    if (!event.target.matches(".header__toggle")) {
+      menuBtn.classList.remove("active");
+      menuLinks.classList.remove("active");
+    }
+  };
 };
 
 // On Scroll Resize Header
 const resizeHeader = () => {
   let y = window.scrollY;
   if (y >= 10) {
-    header.add("active");
+    header.classList.add("active");
   } else {
-    header.remove("active");
+    header.classList.remove("active");
   }
+  menuBtn.classList.remove("active");
+  menuLinks.classList.remove("active");
 };
 
 window.onscroll = resizeHeader;
